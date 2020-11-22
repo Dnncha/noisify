f6451015545307ff058d0c7a9751a3bd8b3d8c29
Donncha <donncha@focalise.ie>
2020-11-22 23:26:37 +0000

    Clean up IP card to /components

--- noisetime/noisetime/src/App.js
+++ noisetime/noisetime/src/App.js
@@ -4,52 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
-class IPCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch
-    ("https://api.ipify.org/?format=json")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          isLoaded: true,
-          items: result
-        });
-      },
-
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div class="mt-4">Your IP is {items.ip}</div>
-      );
-    }
-  }
-}
-
+import IPCard from './components/IPCard'
 
 
 class App extends React.Component {
---
+++ noisetime/noisetime/src/components/IPCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+class IPCard extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch
+      ("https://api.ipify.org/?format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+  
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <div class="mt-4">Your IP is {items.ip}</div>
+        );
+      }
+    }
+  }
+  
+export default IPCard;
